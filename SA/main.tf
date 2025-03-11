2a850b42a9cd188bfb3593cc632ed7c32d0d2fb5
wiadro <warsztat-f@warsztat.pl>
2025-03-11 10:02:03 +0000

    refactor2

--- SA/main.tf
+++ SA/main.tf
@@ -14,10 +14,6 @@ provider "azurerm" {
   features {
     
   }
-  client_id       = "3c0aeb3c-f046-44af-8241-dc5508dc5d4d"     # appId
-  client_secret   = "HSI8Q~lnhS5DXROWRH1zwvdmiGv3qTZN-jmjObum" # password
-  tenant_id       = "dad88b20-14ed-40fc-bdf0-07032373d636"     # tenant
-  subscription_id = "f6ea3f89-3ee9-4c9c-906d-08b5cd8e4730"     # subscription_id
 }
 
 resource "random_pet" "random_f" {
