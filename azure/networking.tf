cd80004e03ea303daeba6aca112bc7492fbb260c
wiadro <warsztat-f@warsztat.pl>
2025-03-11 08:38:45 +0000

    add subnets

--- azure/networking.tf
+++ azure/networking.tf
@@ -9,3 +9,17 @@ resource "random_pet" "random_f" {
   prefix = "pekao"
   length = 1
 }
+
+resource "azurerm_subnet" "subnet_f1" {
+  name = "${random_pet.random_f.id}_subnet-0"
+  resource_group_name = "terraform-training-${var.enviroment}-rg"
+  virtual_network_name = azurerm_virtual_network.virt_network.name
+  address_prefixes = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "subnet_f2" {
+  name = "${random_pet.random_f.id}_subnet-1"
+  resource_group_name = "terraform-training-${var.enviroment}-rg"
+  virtual_network_name = azurerm_virtual_network.virt_network.name
+  address_prefixes = ["10.0.1.0/24"]
+}
