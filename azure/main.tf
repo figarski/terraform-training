e474145ecf3930d17b8926346da704ada2d84f64
wiadro <warsztat-f@warsztat.pl>
2025-03-11 10:00:54 +0000

    refactor

--- azure/main.tf
+++ azure/main.tf
@@ -14,8 +14,6 @@ terraform {
       storage_account_name = "tfstatepekaoserval"
       container_name = "tfstatecointainer"
       key = "pekao_warsztat_tfstatedev"
-      subscription_id = "f6ea3f89-3ee9-4c9c-906d-08b5cd8e4730"
-      tenant_id       = "dad88b20-14ed-40fc-bdf0-07032373d636"
     }
 
 }
@@ -24,10 +22,6 @@ provider "azurerm" {
   features {
     
   }
-  client_id       = "3c0aeb3c-f046-44af-8241-dc5508dc5d4d"     # appId
-  client_secret   = "HSI8Q~lnhS5DXROWRH1zwvdmiGv3qTZN-jmjObum" # password
-  tenant_id       = "dad88b20-14ed-40fc-bdf0-07032373d636"     # tenant
-  subscription_id = "f6ea3f89-3ee9-4c9c-906d-08b5cd8e4730"     # subscription_id
 }
 
 
