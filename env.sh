4ca7010d23318b69dcb70e3d114d69fcce5ff226
Damian Fajfer <damian@fajfer.org>
2025-03-13 10:07:49 +0100

    Dodanie f przed sub id

--- env.sh
+++ env.sh
@@ -5,5 +5,5 @@ export GOOGLE_APPLICATION_CREDENTIALS='/home/student/gcloud.json'
 
 export ARM_CLIENT_ID="3c0aeb3c-f046-44af-8241-dc5508dc5d4d"
 export ARM_CLIENT_SECRET="HSI8Q~lnhS5DXROWRH1zwvdmiGv3qTZN-jmjObum"
-export ARM_SUBSCRIPTION_ID="6ea3f89-3ee9-4c9c-906d-08b5cd8e4730"
+export ARM_SUBSCRIPTION_ID="f6ea3f89-3ee9-4c9c-906d-08b5cd8e4730"
 export ARM_TENANT_ID="dad88b20-14ed-40fc-bdf0-07032373d636"
